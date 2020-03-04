93eafe12396b79429a628f3d8d2fce053224ff74
cksrlcks <chanki.kim89@gmail.com>
2020-03-04 12:06:17 +0900

    add load more button

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -10,8 +10,12 @@ const override = css`
     text-align:center;
 `
 const GET_SEARCH_POSTS = gql`
-    query getSearchPosts($searchQuery: String!){
-        posts(where : {search: $searchQuery}){
+    query getSearchPosts($cursor:String, $searchQuery: String!){
+        posts(first:5, after:$cursor, where : {search: $searchQuery}){
+            pageInfo{
+                endCursor
+                hasNextPage
+            }
             edges{
                 node{
                     postId
@@ -20,20 +24,55 @@ const GET_SEARCH_POSTS = gql`
                     date
                 }
             }
+            __typename
         }
     }
 `;
 
 const PostsList = ({ searchQuery }) => {
-    const { loading, error, data } = useQuery(GET_SEARCH_POSTS, {
-        variables: { searchQuery }
+    const { loading, error, data, fetchMore } = useQuery(GET_SEARCH_POSTS, {
+        variables: { 
+            after:null,
+            searchQuery
+         }
     })
+
+    
+
+    const loadMore = () => {
+        
+        fetchMore({
+            variables:{
+                cursor: data.posts.pageInfo.endCursor
+            },
+            updateQuery: (prev, { fetchMoreResult }) => {
+                const newEdges = fetchMoreResult.posts.edges;
+                const pageInfo = fetchMoreResult.posts.pageInfo;
+                if (!fetchMoreResult) {
+                    return prev;
+                }
+                return {
+                    posts: {
+                        pageInfo,
+                        edges: [...prev.posts.edges, ...newEdges],
+                        __typename: "RootQueryToPostConnectionEdge"
+                    }
+
+                }
+            }
+        })
+    }
 
     if (loading) return <div className="alert_box"><BeatLoader size={10} css={override} color={"#01c080"} /> </div>;
     if (error) return <div className="alert_box">오류가 있습니다.</div>;
     if (!data.posts.edges.length) return <div className="alert_box">검색결과가 없습니다.</div>
 
-    return data.posts.edges.map(post => <PostCard key={post.node.postId} post={post} />)
+    return (
+        <div>
+            {data.posts.edges.map(post => <PostCard key={post.node.postId} post={post} />)}
+            {data.posts.pageInfo.hasNextPage && <button onClick={loadMore} className="load_more">더보기</button>}
+        </div>
+    )
 
 }
 
